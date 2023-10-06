6d3aabc7fee8427e97e453d8d29c4105b10f1c45
Elder-Ferreira <ferreira3.elder@gmail.com>
2023-10-05 22:33:57 -0300

    Configurando campo de identificação do usuario

--- main.py
+++ main.py
@@ -3,9 +3,37 @@ import flet as ft
 def main(pagina):
     texto = ft.Text("Hashzap")
 
+    chat = ft.Column()
+
+    nome_usuario = ft.TextField(label="Escreva seu nome")
+
+    campo_mensagem = ft.TextField(label="Digite uma mensagem")
+    botao_enviar_mensagem = ft.ElevatedButton("Enviar")
+
+    def entrar_popup(evento):
+        pagina.add(chat)
+        popup.open = False
+        pagina.remove(botao_iniciar)
+        pagina.remove(texto)
+        pagina.add(ft.Row(
+            [campo_mensagem, botao_enviar_mensagem]
+        ))
+        pagina.add(botao_enviar_mensagem)
+
+        pagina.update()
+
+    popup = ft.AlertDialog(
+        open=False,
+        modal=True,
+        title=ft.Text("Bem vindo ao Hashzap"),
+        content=nome_usuario,
+        actions=[ft.ElevatedButton("Entrar", on_click=entrar_popup)]
+    )
+
     def entrar_chat(evento):
-        texto_entrou = ft.Text("Entrou no chat")
-        pagina.add(texto_entrou)
+        pagina.dialog = popup
+        popup.open = True
+        pagina.update()
 
     botao_iniciar = ft.ElevatedButton("Iniciar chat", on_click=entrar_chat)
 
@@ -13,3 +41,4 @@ def main(pagina):
     pagina.add(botao_iniciar)
 
 ft.app(target=main, view=ft.WEB_BROWSER)
+
