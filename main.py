e625f8c2f6c1dccd2d3fa1517438e1027ae3e253
Elder-Ferreira <ferreira3.elder@gmail.com>
2023-10-19 18:30:49 -0300

    Identificando usuários do webchat

--- main.py
+++ main.py
@@ -9,21 +9,30 @@ def main(pagina):
     nome_usuario = ft.TextField(label="Escreva seu nome")
 
     def enviar_mensagem_tunel(mensagem):
-        chat.controls.append(ft.Text(mensagem))        
+        tipo = mensagem["tipo"]
+        if tipo == "mensagem":
+            texto_mensagem = mensagem["texto"]
+            usuario_mensagem = mensagem["usuario"]
+            chat.controls.append(ft.Text(f"{usuario_mensagem}: {texto_mensagem}"))    
+        else:
+            usuario_mensagem = mensagem["usuario"]
+            chat.controls.append(ft.Text(f"{usuario_mensagem} entrou no chat", 
+                                         size=12, italic=True, color=ft.colors.ORANGE_500))   
         pagina.update()
 
     pagina.pubsub.subscribe(enviar_mensagem_tunel)
 
     def enviar_mensagem(evento):
-        pagina.pubsub.send_all(campo_mensagem.value)
+        pagina.pubsub.send_all({"texto": campo_mensagem.value, "usuario": nome_usuario.value, "tipo": "mensagem"})
         campo_mensagem.value = ""
         pagina.update()
 
 
-    campo_mensagem = ft.TextField(label="Digite uma mensagem")
+    campo_mensagem = ft.TextField(label="Digite uma mensagem", on_submit=enviar_mensagem)
     botao_enviar_mensagem = ft.ElevatedButton("Enviar", on_click=enviar_mensagem)
 
     def entrar_popup(evento):
+        pagina.pubsub.send_all({"usuario": nome_usuario.value, "tipo": "entrada"})
         pagina.add(chat)
         popup.open = False
         pagina.remove(botao_iniciar)
